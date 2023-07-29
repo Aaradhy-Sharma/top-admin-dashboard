74bbf40ee8005660162efb1475ebc96bf1941a96
Aaradhy Sharma <as783@snu.edu.in>
2023-07-29 18:34:00 +0530

    js code update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const sidebarList1 = document.querySelector('[data-sidebarList1');
+const sidebarList2 = document.querySelector('[data-sidebarList2');
+const dbBottomImg = document.querySelector('[data-dashboardBottomImg');
+const searchBar = document.querySelector('[data-search-bar');
+const searchIcon = documnet.querySelector('[data-search-icon]');
+const langSelector = document.querySelector('[data-lang-selector]');
+const userIcon = document.querySelector('[data-userIcon]');
+const userImg = document.querySelector('[data-userIcon]');
+
+
 console.log("script loaded successfully.")
